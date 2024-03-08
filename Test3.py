cca7839561adc27a12242baf4a2461403ccfd076
KungFuTurtle <m.rfischer5235@gmail.com>
2024-03-08 12:59:55 -0600

    fist day of python

---
+++ Test3.py
@@ -0,0 +1,7 @@
+birth_year = input ("Enter your birth year: ")
+age = 2024 - int(birth_year)
+print(age) 
+int()#"for converting a value into a whole #"
+float()#"for converting a value into decemel #"
+bool()#"converting a value into a ture false statment aka boolen"
+str()#"converting a value into a string"
